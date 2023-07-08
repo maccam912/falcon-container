df808aed6373d92a439d2441c2503a083e46d490
Matt Koski <maccam912@gmail.com>
2023-07-07 19:54:36 -0500

    fix path

--- startup.sh
+++ startup.sh
@@ -8,4 +8,4 @@ if [ ! -f /app/models/wizardlm-uncensored-falcon-40b.ggccv1.q5_k.bin ]; then
     popd
 fi
 
-/user/local/bin/falcon_main -t 24 -n 200 -m /app/models/wizardlm-uncensored-falcon-40b.ggccv1.q5_k.bin -p 8080
+/usr/local/bin/falcon_main -t 24 -n 200 -m /app/models/wizardlm-uncensored-falcon-40b.ggccv1.q5_k.bin -p 8080
