0960cce84307971e5851eed32b41379085632120
Matt Koski <maccam912@gmail.com>
2023-07-08 15:49:06 -0500

    back to 40b

--- startup.sh
+++ startup.sh
@@ -3,11 +3,11 @@ set -ex
 
 # check if /app/models/wizardlm-uncensored-falcon-40b.ggccv1.q5_k.bin exists. If not, run wget
 # check if /app/models/wizardlm-7b-uncensored.ggccv1.q8_0.bin exists. If not, run wget
-if [ ! -f /app/models/wizardlm-7b-uncensored.ggccv1.q8_0.bin ]; then
-# if [ ! -f /app/models/wizardlm-uncensored-falcon-40b.ggccv1.q5_k.bin ]; then
+# if [ ! -f /app/models/wizardlm-7b-uncensored.ggccv1.q8_0.bin ]; then
+if [ ! -f /app/models/wizardlm-uncensored-falcon-40b.ggccv1.q5_k.bin ]; then
     pushd /app/models
-    wget https://huggingface.co/TheBloke/WizardLM-Uncensored-Falcon-7B-GGML/resolve/main/wizardlm-7b-uncensored.ggccv1.q8_0.bin
-    # wget https://huggingface.co/TheBloke/WizardLM-Uncensored-Falcon-40B-GGML/resolve/main/wizardlm-uncensored-falcon-40b.ggccv1.q5_k.bin
+    # wget https://huggingface.co/TheBloke/WizardLM-Uncensored-Falcon-7B-GGML/resolve/main/wizardlm-7b-uncensored.ggccv1.q8_0.bin
+    wget https://huggingface.co/TheBloke/WizardLM-Uncensored-Falcon-40B-GGML/resolve/main/wizardlm-uncensored-falcon-40b.ggccv1.q5_k.bin
     popd
 fi
 
