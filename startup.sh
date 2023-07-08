2ff4012a157808794945784303ac6a2ef5268706
Matt Koski <maccam912@gmail.com>
2023-07-07 20:02:11 -0500

    start

--- startup.sh
+++ startup.sh
@@ -8,4 +8,4 @@ if [ ! -f /app/models/wizardlm-uncensored-falcon-40b.ggccv1.q5_k.bin ]; then
     popd
 fi
 
-/usr/local/bin/falcon_main -t 24 -n 200 -m /app/models/wizardlm-uncensored-falcon-40b.ggccv1.q5_k.bin -p 8080
+/usr/local/bin/falcon_main -t 11 -n 32 -m /app/models/wizardlm-uncensored-falcon-40b.ggccv1.q5_k.bin -p "The answer is"
